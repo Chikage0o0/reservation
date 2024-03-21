e12ea1a5f1cfbf05825360045322780f5e17fe45
Chikage <chikage@939.me>
2024-03-21 06:58:46 +0000

    Rename protobuf enum let tonic_build can auto cut the enum name

--- abi/src/lib.rs
+++ abi/src/lib.rs
@@ -4,17 +4,4 @@ mod pb {
     tonic::include_proto!("reservation");
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use pb::*;
